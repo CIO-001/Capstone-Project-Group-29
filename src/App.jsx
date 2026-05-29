8f7f127ae0804881c179de7d8a8bb1a80d06755f
covenantudofe-creator <covenantudofe@gmail.com>
2026-05-28 23:01:19 -0700

    Added structure and styling for the hero section
    Successfully built the Hero section using JSX and styled it with CSS for better layout and responsiveness.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import Hero from './components/Hero';
 import TableSection from "./TableSection/TableSection";
 import Footer from './Footer/Footer';
+import VideoSection from "./VideoSection/VideoSection";
 
 function App() {
   return (
     <>
 
       <Hero />
+      <VideoSection />
       <TableSection />
       <Footer />
     </>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,64 @@
-// import "./Hero.css";
+
+import "./Hero.css";
 
 export default function Hero() {
   return (
     <section className="hero">
-      <div className="hero-text">
-        <h1>Explore Our Solar System Through Data</h1>
+
+      {/* Logo */}
+      <div className="hero-logo">
+        <img
+          src="/logo.svg"
+          alt=""
+          className="logo-image"
+        />
+      </div>
+
+      {/* Hero Content */}
+      <div className="hero-content">
+
+        <h1>
+          Explore Our Solar
+          <br />
+          System Through Data
+        </h1>
 
         <p>
-          Understand the planets not just by names, but by measureable facts.
+          Understand the planets not just by names,
+          but by measurable facts. From size and mass
+          to gravity and density, this page breaks down
+          the solar system in a clear, data-driven way.
         </p>
 
         <div className="hero-buttons">
-          <button>Explore the Data</button>
-          <button className="outline">Contact Us</button>
+
+          <button
+            type="button"
+            className="primary-btn"
+          >
+            Explore the Data
+          </button>
+
+          <button
+            type="button"
+            className="secondary-btn"
+          >
+            Contact Us
+          </button>
+
         </div>
       </div>
 
+      {/* Planet Globe */}
       <div className="hero-image">
-        <img src="/earth.png" alt="Earth"/>
+        <img
+          src="/earth.png"
+          alt="Planet Earth"
+          className="earth-image"
+        />
       </div>
+
     </section>
   );
 }
+
